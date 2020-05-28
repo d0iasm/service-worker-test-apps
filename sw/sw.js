e2003308962355cc86b5fe814822fe74d831f76d
Asami Doi <asamidoi@chromium.org>
2020-05-28 03:54:57 +0000

    Add event listeners in SW.

--- sw/sw.js
+++ sw/sw.js
@@ -1,1 +1,31 @@
-// Do nothing.
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+	console.log('Fetch event in SW');
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
