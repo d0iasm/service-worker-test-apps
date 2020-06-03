00b270150c2d45d55f2fdf452f515aff069fae5c
Asami Doi <asamidoi@chromium.org>
2020-06-03 04:50:17 +0000

    Add test app for navigation preload.

---
+++ navpreload/sw.js
@@ -0,0 +1,19 @@
+addEventListener('activate', event => {
+  event.waitUntil(async function() {
+    // Feature-detect
+    if (self.registration.navigationPreload) {
+      // Enable navigation preloads!
+      await self.registration.navigationPreload.enable();
+    }
+  }());
+});
+
+addEventListener('fetch', event => {
+	event.respondWith((async () => {
+  	try {
+    	return await event.preloadResponse;
+    } catch (e) {
+      return new Response("Hello Offline page");
+   	}
+  })());
+});
